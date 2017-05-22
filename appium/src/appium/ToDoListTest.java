a753d5ada5c538a6f3d6e08aa7f07589aa9f2f79
chenxuejiao <chenxuejiao@le.com>
2017-05-22 14:19:06 +0800

    Install and open application

--- appium/src/appium/ToDoListTest.java
+++ appium/src/appium/ToDoListTest.java
@@ -1,10 +1,16 @@
 package appium;
 
+import java.io.File;
 import java.net.MalformedURLException;
 
 public class ToDoListTest {
 
 	public static void main(String[] args) throws MalformedURLException {
+
+		// Install and open application
+		File classpathRoot = new File(System.getProperty("user.dir"));
+		File appDir = new File(classpathRoot, "app/");
+		File app = new File(appDir, "ToDoList.apk");
 
 	}
 
